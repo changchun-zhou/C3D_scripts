d1d7d29e3f8dd833a0d09582cc95454a6816a29d
zhouchch3 <2835079913@qq.com>
2022-01-07 16:04:49 +0800

    feat(train model): to_csv
    add table head

--- Train/auto_threshold/to_csv.py
+++ Train/auto_threshold/to_csv.py
@@ -2,11 +2,21 @@ import random
 import pandas as pd
 from datetime import datetime
 import numpy as np
+import os
 #创建train_acc.csv和var_acc.csv文件，记录loss和accuracy
 # df = pd.DataFrame(columns=['time','step','train Loss','training accuracy'])#列名
 
 #初始化train数据
 def to_csv(CSV_PATH, t_step, list_write):
+    
+    if not os.path.exists(CSV_PATH):
+        data = pd.DataFrame(columns=['time','step','accuracy',\
+            'threshold0', 'threshold1','threshold2','threshold3','threshold4','threshold5','threshold6', 'threshold7', \
+            'epoch_loss', 'loss_weight', 'loss_th',\
+            'lr0', 'lr1', 'lr_th',\
+            'scale0', 'scale1', 'scale2', 'scale3','scale4', 'scale5','scale6', 'scale7'])
+        data.to_csv(CSV_PATH,mode='a',index=False)
+    
     time = "%s"%datetime.now()#获取当前时间
     step = "Step[%d]"%t_step
     for i in range(len(list_write)):
