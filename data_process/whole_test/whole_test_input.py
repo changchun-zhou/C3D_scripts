cc7f936c9300ab42b71adba4cef67585b22f46e8
zhouchch3 <2835079913@qq.com>
2022-02-11 10:33:27 +0800

    feat(whole_test):
    1. fix abs(input_quants);
    2. add return num_patch num_frame

--- data_process/whole_test/whole_test_input.py
+++ data_process/whole_test/whole_test_input.py
@@ -1,4 +1,4 @@
-
+#--coding:utf-8--
 import torch 
 import numpy as np 
 import os
@@ -40,11 +40,11 @@ def tensor_to_file_act(output_dir, tensor,threshold):
     fp_info = open(os.path.join(output_dir)+'/'+'config'+'.txt','a') 
     # Num_patch =  (math.ceil(shape[3]/16.0))* (math.ceil(shape[4]/16.0))
     Num_patch_H = math.ceil(shape[3]/16.0)
-    Num_patch_W = math.ceil(shape[3]/16.0)
+    Num_patch_W = math.ceil(shape[4]/16.0)
     Num_patch = Num_patch_H * Num_patch_W
     Num_frame = shape[2]
     Num_block = math.ceil(shape[1]/32)
-    tensor = torch.nn.functional.pad(tensor, (0, 16*math.ceil(shape[4]/16)-shape[4], 0, 16*math.ceil(shape[3]/16) - shape[3], \
+    tensor = torch.nn.functional.pad(torch.where(abs(tensor)>=threshold, tensor + 1, torch.zeros(shape).to(tensor.device)), (0, 16*math.ceil(shape[4]/16)-shape[4], 0, 16*math.ceil(shape[3]/16) - shape[3], \
         0, math.ceil(shape[2]/1)-shape[2], 0, 32*math.ceil(shape[1]/32)-shape[1] ) )
     array_shape = (tensor.cpu()).detach().numpy()
 
@@ -55,11 +55,11 @@ def tensor_to_file_act(output_dir, tensor,threshold):
                     for H in range(16):
                         for W in range(16):
                             for chn in range(32):
-                                tmp = int(array_shape[0][chn + 32*block][frame][H + 16*patch_h][W + 16*patch_w])
-                                if abs(tmp) >= threshold :
+                                tmp = array_shape[0][chn + 32*block][frame][H + 16*patch_h][W + 16*patch_w]
+                                if abs(tmp) > 0 :
                                     cnt_element += 1
                                     flag = 1
-                                    cnt_wr_data, temp_data= WRITE_BACK(fp_data_wr, 32, 2, tmp, cnt_wr_data, temp_data)
+                                    cnt_wr_data, temp_data= WRITE_BACK(fp_data_wr, 32, 2, int(tmp), cnt_wr_data, temp_data)
                                 else:
                                     flag = 0
                                 cnt_wr_flag, temp_flag = WRITE_BACK(fp_flag_wr, 128,1, flag, cnt_wr_flag, temp_flag)
@@ -74,12 +74,11 @@ def tensor_to_file_act(output_dir, tensor,threshold):
     fp_info.write('\nconfig CFGGB_num_alloc/total_act    = '+ str(numblk_act)+'rest:'+str(cnt_element%(16*512)))
     fp_info.write('\nconfig CFGGB_num_loop_wei = '+str(Num_frame))
     for zero_pad in range( (16*512) - cnt_element%(16*512) ):
-        tmp = 0
-        cnt_wr_data, temp_data= WRITE_BACK(fp_data_wr, 32, 2, tmp, cnt_wr_data, temp_data)
+        cnt_wr_data, temp_data= WRITE_BACK(fp_data_wr, 32, 2, 0, cnt_wr_data, temp_data)
     for zero_pad_flag in range( (128*512) - cnt_element_flag%(128*512) ):
         flag = 0
         cnt_wr_flag, temp_flag = WRITE_BACK(fp_flag_wr, 128,1, flag, cnt_wr_flag, temp_flag)
-    return numblk_flaact, numblk_act
+    return numblk_flaact, numblk_act, Num_patch, Num_frame, Num_block
 
 def tensor_to_file_wei(output_dir,tensor, threshold):
 
@@ -111,7 +110,7 @@ def tensor_to_file_wei(output_dir,tensor, threshold):
                         cnt_wr_addrwei, temp_addrwei= WRITE_BACK(fp_addrwei_wr, 32, 4, int(addr_element/16), cnt_wr_addrwei, temp_addrwei)
                         for chn in range(shape[1]):
                             tmp = int(array_shape[weight + 16*patch][chn][frame][H][W])
-                            if abs(tmp) >= threshold :
+                            if abs(tmp) >= threshold:
                                 cnt_element += 1
                                 cnt_sparsity += 1
                                 addr_element += 1
@@ -143,11 +142,11 @@ def tensor_to_file_wei(output_dir,tensor, threshold):
     numblk_wei = math.ceil(cnt_element/(16*512))
     fp_info.write('\nconfig CFGGB_num_alloc/total_wei = ' + str(numblk_wei)+'rest:'+str(cnt_element%(16*512)))
     fp_info.write('\nconfig CFGGB_num_loop_act = '+ str(int(shape[0]/16.0)))
-    return numblk_weiaddr, numblk_flgwei, numblk_wei
+    return numblk_weiaddr, numblk_flgwei, numblk_wei, int(shape[0]/16.0)
 
 # random.seed(000)
 # dict = {
-#         'norm_conv1'     :{'activation':torch.round( torch.normal(0.108, torch.ones(8,   3, 16,  16,  16)*0.430) ), 'weight': torch.round( torch.normal(-0.137,  torch.ones(64 ,  3, 3, 3, 3)*0.430) )}, # 0.3818
+        # 'norm_conv1'     :{'activation':torch.round( torch.normal(0.108, torch.ones(8,   3, 16,  16,  16)*0.430) ), 'weight': torch.round( torch.normal(-0.137,  torch.ones(64 ,  3, 3, 3, 3)*0.430) )}, # 0.3818
 #         'norm_conv2'     :{'activation':torch.round( torch.normal(0.108, torch.ones(8,  64, 16,  16,  16)*0.430) ), 'weight': torch.round( torch.normal(-0.137,  torch.ones(128, 64, 3, 3, 3)*0.430) )},
         # 'norm_conv3'     :{'activation':torch.round( torch.normal(0.108, torch.ones(8, 128,  8,  16,  16)*0.430) ), 'weight': torch.round( torch.normal(-0.137,  torch.ones(256,128, 3, 3, 3)*0.430) )},
         # 'norm_conv4'     :{'activation':torch.round( torch.normal(0.108, torch.ones(8, 256,  8,  16,  16)*0.430) ), 'weight': torch.round( torch.normal(-0.137,  torch.ones(256,256, 3, 3, 3)*0.430) )},
@@ -201,10 +200,23 @@ weight_dict = torch.load(path_weight)
 for key, value in weight_dict.items():
     print(key, value.size()) 
 activation_dict = torch.load(path_activation)
+# baseline的稀疏度是数据全密然后安排按硬件计算需要填充0到32x16x16后的稀疏度：需要填充的是conv1 conv7 conv8
+# 
 dict = {
-        # 'norm_conv1'     :{'activation': activation_dict['inputs_quant'][:][:][0:16][0:16][0:16]*activation_dict['scale'][0], 'weight': weight_dict['module.conv1.float_weight']}, # 0.3818
-        # 'norm_conv2'     :{'activation': activation_dict['pool1'][:][:][0:16][0:16][0:16]*activation_dict['scale'][1], 'weight': weight_dict['module.conv2.float_weight']},
-        'norm_conv3'     :{'activation': activation_dict['pool2'][:, :, :, 0:16, 0:16]*activation_dict['scale'][2], 'weight': weight_dict['module.conv3a.float_weight'][0:64]*263.7711, 'threshold_activation': 10, 'threshold_weight': 10}
+        # 'norm_conv1_92x77'     :{'activation': activation_dict['inputs_quant'][:, :, : , 0:32, 0:32]*activation_dict['scale'][0], 'weight': weight_dict['module.conv1.float_weight']*151.7562, 'threshold_activation': 5, 'threshold_weight': 50}, # 0.3818
+        'baseline_conv1'     :{'activation': activation_dict['inputs_quant'][:, :, : , 0:16, 0:32]*activation_dict['scale'][0], 'weight': weight_dict['module.conv1.float_weight']*151.7562, 'threshold_activation': 0, 'threshold_weight': 0}, 
+        # 'norm_conv2_98x96'     :{'activation': activation_dict['pool1'][:, :, :, 0:16, 0:16]*activation_dict['scale'][1], 'weight': weight_dict['module.conv2.float_weight'][0:64]*228.5491, 'threshold_activation': 9, 'threshold_weight': 14},
+        # 'norm_conv3'     :{'activation': activation_dict['pool2'][:, :, :, 0:16, 0:16]*activation_dict['scale'][2], 'weight': weight_dict['module.conv3a.float_weight'][0:64]*263.7711, 'threshold_activation': 10, 'threshold_weight': 10},
+        # 'baseline_conv3'     :{'activation': activation_dict['pool2'][:, 0:64, 0:4, 0:16, 0:16]*activation_dict['scale'][2], 'weight': weight_dict['module.conv3a.float_weight'][0:16, 0: 64]*263.7711, 'threshold_activation': 0, 'threshold_weight': 0}
+        # 'norm_conv3_98x90'     :{'activation': activation_dict['pool2'][:, :, :, 0:16, 0:16]*activation_dict['scale'][2], 'weight': weight_dict['module.conv3a.float_weight'][0: 64]*263.7711, 'threshold_activation': 5, 'threshold_weight': 10}
+        # 'norm_conv4_98x87'     :{'activation': activation_dict['relu3'][:, :, 0:4, 0:16, 0:16]*activation_dict['scale'][3], 'weight': weight_dict['module.conv3b.float_weight'][0: 32]*212.5621, 'threshold_activation': 6, 'threshold_weight': 5}
+        # 'baseline_conv4'     :{'activation': activation_dict['relu3'][:, 0:64, 0:4, 0:16, 0:16]*activation_dict['scale'][3], 'weight': weight_dict['module.conv3b.float_weight'][0:16, 0: 64]*212.5621, 'threshold_activation': 0, 'threshold_weight': 0}
+        # 'hlb_conv5_97x88'     :{'activation': activation_dict['pool3'][:, :, 0:4, 0:16, 0:16]*activation_dict['scale'][4], 'weight': weight_dict['module.conv4a.float_weight'][0: 32]*288.2065, 'threshold_activation': 6, 'threshold_weight': 6}
+        # 'hlb_conv6_97x83'     :{'activation': activation_dict['relu5'][:, :, 0:2, 0:16, 0:16]*activation_dict['scale'][5], 'weight': weight_dict['module.conv4b.float_weight'][0: 16]*350.2377, 'threshold_activation': 3, 'threshold_weight': 4}
+        # 'hlb_conv7_98x97'     :{'activation': activation_dict['pool4'][:, :, 0:4, 0:16, 0:16]*activation_dict['scale'][6], 'weight': weight_dict['module.conv5a.float_weight'][0: 32]*587.4043, 'threshold_activation': 4, 'threshold_weight': 9}
+        # 'baseline_conv7'     :{'activation': activation_dict['pool4'][:, 0:128, 0:4, 0:16, 0:16]*activation_dict['scale'][6], 'weight': weight_dict['module.conv5a.float_weight'][0:16, 0:128]*587.4043, 'threshold_activation': 0, 'threshold_weight': 0}
+        # 'hlb_conv8_98x90'     :{'activation': activation_dict['relu7'][:, :, 0:4, 0:16, 0:16]*activation_dict['scale'][7], 'weight': weight_dict['module.conv5b.float_weight'][0: 32]*626.8743, 'threshold_activation': 0.5, 'threshold_weight': 10}
+        # 'baseline_conv8'     :{'activation': activation_dict['relu7'][:, 0:128, 0:4, 0:16, 0:16]*activation_dict['scale'][7], 'weight': weight_dict['module.conv5b.float_weight'][0: 16, 0:128]*626.8743, 'threshold_activation': 0, 'threshold_weight': 0}
 }
 
 for case, params in dict.items():
@@ -214,13 +226,14 @@ for case, params in dict.items():
     
     # tensor = torch.ones([8,  64, 16,  14,  14])
     print("activation shape: {}".format(params['activation'].size()))
-    numblk_flgact, numblk_act = tensor_to_file_act(output_dir=discrete_dir,tensor=params['activation'], threshold=params['threshold_activation']) # >= theshold
+    numblk_flgact, numblk_act, Num_patch, Num_frame, Num_block = tensor_to_file_act(output_dir=discrete_dir,tensor=params['activation'], threshold=params['threshold_activation']) # >= theshold
 
     # tensor = torch.ones([32, 64, 3, 3, 3 ])
     print("weight shape: {}".format(params['weight'].size()))
-    numblk_weiaddr, numblk_flgwei, numblk_wei = tensor_to_file_wei(output_dir=discrete_dir, tensor=params['weight'], threshold=params['threshold_weight'])
+    numblk_weiaddr, numblk_flgwei, numblk_wei, num_ftrgrp = tensor_to_file_wei(output_dir=discrete_dir, tensor=params['weight'], threshold=params['threshold_weight'])
     ROM_dir = os.path.join('output', 'c3d', case, 'ROM')
     if not os.path.exists(ROM_dir):
         os.makedirs(ROM_dir)
-    cls_gen_coe(discrete_dir, ROM_dir).func_gen_coe(numblk_weiaddr, numblk_flgwei, numblk_wei, numblk_flgact, numblk_act)
+    cls_gen_coe(discrete_dir, ROM_dir).func_gen_coe(numblk_weiaddr, numblk_flgwei, numblk_wei, numblk_flgact, numblk_act, \
+        CFGCCU_num_layer = 0, CFGCCU_num_patch = Num_patch-1, CFGCCU_num_ftrgrp= num_ftrgrp-1, CFGCCU_num_frame = Num_frame-1, CFGCCU_num_block = Num_block-1)
     print(numblk_weiaddr, numblk_flgwei, numblk_wei, numblk_flgact, numblk_act)
